0c55532ad706c7a70d22166c83d288d4f77e6b5d
Mahij <51525539+khallnayak@users.noreply.github.com>
2020-04-13 11:12:13 +0530

    Add files via upload

---
+++ DUCE_original/seeder.py
@@ -0,0 +1,84 @@
+from Node import Seeder
+import socket as s
+import threading as th
+import json
+import time
+
+S=s.socket(s.AF_INET,s.SOCK_DGRAM)
+HOST=s.gethostbyname(s.gethostname())
+S.setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR,1)
+S.bind((HOST,6969))
+peer_threads={}
+sockets=[]
+
+"""
+INIT FILE NAME AND GENERATE BLOCKS WITH LEDGER
+ASK FOR TIER SIZE
+"""
+
+seeder=Seeder(3)
+
+def refresh(soc,chain,addr):
+	try:
+		soc.sendto(json.dumps(chain).encode(),addr)
+		return True
+	except OSError as e:
+		return False
+
+
+def seed(soc,addr):
+	msg="ORA ORA ORA".encode()
+	flag,number=seeder.allocate_number()
+	soc.sendto(json.dumps(number).encode(),addr)
+	print(soc)
+	if(type(number)==int):
+		answer,ans_addr=soc.recvfrom(40)
+		print(answer,ans_addr)
+		if(answer):
+			seeder.init_connection(addr)
+			soc.sendto(json.dumps(seeder.size).encode(),ans_addr)
+			flag=refresh(soc,seeder.get_chain(),ans_addr)
+			"""
+			SEND LEDGER SHIT
+			"""
+			if(not flag):
+				#Something error
+				print("FLAG ERROR",flag)
+		else:
+			soc.close()
+			print("NO ANSWER FROM PEER")
+		time1=time.time()
+		while True:
+			try:
+				if(time.time()-time1>=5):
+					soc.sendto("CHAIN".encode(),ans_addr)
+					chain,_=soc.recvfrom(2048)
+					chain=chain.decode()
+					print(chain,"\t FROM PEER",number)
+					seeder.update_chain(chain,number)
+					refresh(soc,seeder.get_chain(),ans_addr)
+					print("CHAIN UPDATE")
+					time1=time.time()
+				else:
+					"""
+					1. SELECT BLOCK NUMBER AND SEND BLOCK
+					2. BLOCK CHECKING ON PEER SIDE
+					"""
+					soc.sendto(msg,ans_addr)
+					print(soc.recvfrom(1024),"IN THREAD",sep="\t")
+				time.sleep(0.2)
+			except OSError as e:
+				print(e)
+while True:
+	thread=[]
+	try:
+		data,addr=S.recvfrom(1024)
+		print(data,addr,"IN MAIN",sep="\t")
+		if(addr not in peer_threads.keys()):
+			sockets.append(s.socket(s.AF_INET,s.SOCK_DGRAM))
+			sockets[-1].setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR,1)
+			thread.append(th.Thread(target=seed,args=(sockets[-1],addr,)))
+			thread[-1].start()
+	except:
+		pass
+		
---
+++ DUCE_original/temp_peer.py
@@ -0,0 +1,210 @@
+from Node import Peer
+import socket as s
+import threading as th
+import json
+import sys
+import time
+import random
+
+
+
+#S=s.socket(s.AF_INET,s.SOCK_DGRAM)
+HOST=s.gethostbyname(s.gethostname())
+
+#S.setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR,1)
+
+
+P=s.socket(s.AF_INET,s.SOCK_DGRAM)
+P.setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR,1)
+P.bind((HOST,6971))
+
+peer_obj=Peer()
+
+def inter_peer(seeder_chain,soc,i,ip):
+	print(seeder_chain,ip,"IN INTER_PEER",sep="\t")
+	"""
+	HOW TO AUTHENTICATE THE NEW SOCKET AS ORIGINAL PEER AND NO ONE ELSE?
+	"""
+	try:
+		soc.sendto(json.dumps(peer_obj.number).encode(),tuple(ip))
+		data,ip=soc.recvfrom(1024)
+	except:
+		print("Error connecting to peer",ip)
+	
+	msg=b"ARI ARI ARI"
+	while True:
+		try:
+			soc.sendto(msg,ip)
+			print(soc.recvfrom(1024),"FROM CHOTU "+str(i),sep="\t")
+		except OSError as e:
+			print("THREAD ERROR OCCURED",e)
+			break
+	while True:
+		try:
+			"""
+			GET TOTAL BLOCKS NUMBER
+			"""
+			number=10
+			required_blocks=set(range(number)).difference(seeder_chain["Data"])
+			req=random.choice(required_blocks)
+
+			soc.sendto(str(req).encode(),ip)
+
+			seeder_chain["Data"].append(req)
+
+			data,_=soc.recvfrom(1024)
+			data=data.decode()
+			"""
+			Data will contain block number
+			"""
+			while data!="OVERIDA".encode():
+				try:
+					data,_=soc.recvfrom(1024)
+				except:
+					print("FIle sending error. Fix SHIT")
+			"""
+			LOAD FILE INTO VARIABLE
+			"""
+			file=b"DORA RARA RARA"
+			ctr=int(len(block)/1024)+1
+
+			"""
+			FURTHER PART OF CHECKING THE BLOCK TRANSFERS
+			history=[False]*ctr
+			After a block has successfully transfered to the peer, history[block_number]=True
+			If error happens the history can be used to resend the block
+			"""
+			
+
+			while ctr!=0:
+				"""
+				LOAD BLOCK INTO VARIABLE
+				"""
+				block=file
+				try:
+					soc.sendto(block,ip)
+				except:
+					"""
+					ADDING THE ERROR AND INDEX OF ERROR BlOCK IN HISTORY
+					"""
+					pass
+
+		except:
+			pass
+
+def process_chain(chain,soc):
+	chain,ip_table=chain["CHAIN"],chain["IP_TABLE"]
+	for i in map(str,range(peer_obj.number+1,peer_obj.size+1)):
+		if(i not in peer_obj.fellow_peer.keys() and chain[i]['Vac']!=-1):
+			peer_obj.fellow_peer[i]=[]
+			peer_obj.fellow_peer[i].append(s.socket(s.AF_INET,s.SOCK_DGRAM))
+			peer_obj.fellow_peer[i][-1].setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR,1)
+			peer_obj.fellow_peer[i].append(th.Thread(target=inter_peer,args=(chain[i],peer_obj.fellow_peer[i][0],i,ip_table[int(i)-1],)))
+			peer_obj.fellow_peer[i][-1].start()
+
+
+
+
+
+def seeder(S,ip,number):
+	print("INSIDE SEEDER THREAD",ip,number,sep='\t')
+	msg="MUDA MUDA MUDA".encode()
+	S.sendto("I AM HERE!".encode(),ip)
+	size,_=S.recvfrom(1024)
+	print(size,"SIZE")
+	size=json.loads(size)
+	peer_obj.update_data(size,number)
+	chain,_=S.recvfrom(1024)
+	chain=json.loads(chain.decode())
+	peer_obj.seeder_chain["CHAIN"],peer_obj.seeder_chain["IP_TABLE"]=json.loads(chain[0]),json.loads(chain[1])
+	print(peer_obj.seeder_chain,"INITIAL D")
+	t1=time.time()
+	ctr=0
+	while True:
+		try:
+			ans,_=S.recvfrom(1024)
+			print(ans)
+
+			if(time.time()-t1>=5):
+				peer_obj.seeder_chain["CHAIN"][str(peer_obj.number)]['Data'].append(ctr)
+				ctr+=1
+				t1=time.time()
+
+			if(ans=="CHAIN".encode()):
+				S.sendto(json.dumps(peer_obj.seeder_chain["CHAIN"][str(peer_obj.number)]).encode(),ip)
+				chain,_=S.recvfrom(2048)
+				chain=json.loads(chain.decode())
+				peer_obj.seeder_chain["CHAIN"],peer_obj.seeder_chain["IP_TABLE"]=json.loads(chain[0]),json.loads(chain[1])
+				print(peer_obj.seeder_chain,"NEW CHAIN")
+				process_chain(peer_obj.seeder_chain,S)
+			else:
+				S.sendto(msg,ip)
+			time.sleep(0.2)
+		except OSError as e:
+			print(e)
+
+def peer(data,addr,soc):
+	msg=b"ARI ARI ARI"
+	while True:
+		try:
+			soc.sendto(msg,addr)
+			print(soc.recvfrom(1024))
+			time.sleep(1)
+		except:
+			break
+
+
+
+
+"""
+FIND SEEDER SHIT
+"""
+Seeder=(HOST,6969)
+seeder_soc=s.socket(s.AF_INET,s.SOCK_DGRAM)
+seeder_soc.setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR,1)
+try:
+	P.sendto("Connect".encode(),Seeder)
+	data,ip=P.recvfrom(1024)
+	data=json.loads(data)
+	print(data,"Seeder answer loaded",type(data))
+	while type(data)!=int:
+		P.sendto("Connect".encode(),data)
+		data,ip=P.recvfrom(1024)
+		data=json.loads(data)
+	seeder_thread=th.Thread(target=seeder,args=(seeder_soc,ip,data,))
+	seeder_thread.start()
+	seeder_thread_active=True
+except:
+	pass
+print("SEEDER DONE")
+while True:
+	try:
+		data,addr=P.recvfrom(1024)
+		data=json.loads(data.decode())
+		print(data,type(data),sep="\t")
+		if(addr not in peer_obj.fellow_peer.keys() and type(data)==int): #### IF INT THEN FELLOW PEER
+			"""
+			IMPLEMENT PEER_OBJ.FELLOW_PEER SHIT AS DONE ABOVE FOR PEER AND ENABLE COMMUNICATION
+			"""
+			print("FELLOW_PEER")
+			socketo=s.socket(s.AF_INET,s.SOCK_DGRAM)
+			socketo.setsockopt(s.SOL_SOCKET,s.SO_REUSEADDR,1)
+			peer_thread=th.Thread(target=peer,args=(data,addr,socketo,))
+			peer_thread.start()
+			peer_obj.fellow_peer[data]=[socketo,peer_thread]
+		if(addr[0] not in peer_obj.fellow_peer.keys() and type(data)==str):##### IF STR THEN DOWN LEVEL PEER
+			print("DOWN LEVEL PEER")
+
+	except OSError as e:
+		print(e)
+		pass
+
+
+
+"""
+NOTES FOR FUTURE IMPLEMENTATION
+
+
+1. Add break statement at the end of except statement in thread
+
+"""
